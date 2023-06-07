9482dbc36f3a5bb7082fb6b0e60ceb6009701725
Priyanshu Panwar <priyanshu009ch@gmail.com>
2023-06-08 00:40:35 +0530

    added project_urls

--- setup.py
+++ setup.py
@@ -7,13 +7,16 @@ with open(readme_file, encoding="utf-8") as f:
 
 setup(
     name="yt-channel-scraper",
-    version="0.0.4",
+    version="0.0.5",
     description="Fetch channel_id, videos, transcript from a youtube channel",
     long_description=readme_content,
     long_description_content_type="text/markdown",
     author="Priyanshu Panwar",
     author_email="priyanshu009ch@gmail.com",
     packages=["yt_channel_scraper"],
+    project_urls={
+        "Source": "https://github.com/priyanshu-panwar/yt-channel-scraper",
+    },
     install_requires=[
         "scrapetube",
         "youtube-transcript-api",
